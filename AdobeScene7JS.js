b732cd88f76a4ac286f5e2367908f0b1a7885ccb
Chris Magnuson <hourglasssand@gmail.com>
2019-09-13 10:30:18 -0400

    Add Out_AdobeScene7URLPrettyPrint

--- AdobeScene7JS.js
+++ AdobeScene7JS.js
@@ -50,3 +50,46 @@ export function New_AdobeScene7URL ({
         return decodeURIComponent($URL.href)
     }
 }
+
+export function Out_AdobeScene7URLPrettyPrint ({
+    $URL
+}) {
+    var $URLDecoded = $URL
+        .replace(/%26/g,"&")
+        .replace(/%27/g,"'")
+        .replace(/%3f/g,"?")
+        .replace(/%7b/g,"{")
+        .replace(/%7d/g,"}")
+
+    var $URLPrettyPrinted = ""
+    var $CharactersToBreakAfter = ["?"]
+    var $CharactersToBreakBefore = ["&"]
+    var $CharactersToIndentOn = ["{","("]
+    var $CharactersToUnindentOn = ["}",")"]
+    var $IndentionLevel = 0
+
+    for (var $Character of  [...$URLDecoded]) {
+        if ($CharactersToBreakAfter.includes($Character)) {
+            $URLPrettyPrinted += `${$Character}\n`
+            $URLPrettyPrinted += "    ".repeat($IndentionLevel)
+        } else if ($CharactersToBreakBefore.includes($Character)) {
+            if (![" ","\n"].includes([...$URLPrettyPrinted].slice(-1)[0])) {
+                $URLPrettyPrinted += "\n"
+                $URLPrettyPrinted += "    ".repeat($IndentionLevel)
+            }
+            $URLPrettyPrinted += $Character
+        } else if ($CharactersToIndentOn.includes($Character)) {
+            $URLPrettyPrinted += `${$Character}\n`
+            $IndentionLevel += 1
+            $URLPrettyPrinted += "    ".repeat($IndentionLevel)
+        } else if ($CharactersToUnindentOn.includes($Character)) {
+            $IndentionLevel -= 1
+            $URLPrettyPrinted += "\n"
+            $URLPrettyPrinted += "    ".repeat($IndentionLevel)
+            $URLPrettyPrinted += $Character
+        } else {
+            $URLPrettyPrinted += $Character
+        }
+    }
+    return $URLPrettyPrinted
+}
